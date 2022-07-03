bb139fad0947fbb89b9e08aed30eb7ed8dd4309f
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-07-03 11:49:12 +0100

    update

--- static/app.js
+++ static/app.js
@@ -1,4 +1,20 @@
+let ul = document.getElementsByClassName("main_body");
+let li = ul.querySelectorAll('.main_body.main_body_jobs')
+let input = document.getElementsByClassName('nav_input');
 const image_input = document.querySelector("#image-input");
+const listArray = Array.from(li);
+console.log('okay')
+const filterUsers = (event) => {
+  ul.innerHTML = ""
+  let keyword = input.value.toLowerCase();
+  listArray.forEach((item) =>  {
+    if(item.textContent.toLowerCase().includes(keyword)) {
+      ul.appendChild(item)
+    }
+  })
+}
+
+input.addEventListener('keyup', filterUsers);
 
 
 image_input.addEventListener("change", function() {
@@ -9,3 +25,5 @@ image_input.addEventListener("change", function() {
     });
     reader.readAsDataURL(this.files[0]);
   });
+
+
